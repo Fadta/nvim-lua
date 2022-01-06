d013d72cb5f30f7282378bb149bf38a3449f49b3
Fadta <fvpan01@gmail.com>
2022-01-06 12:09:45 -0300

    added requirements for fresh systems
    hope I don't die when I migrate to Arch

---
+++ install_lsp.sh
@@ -0,0 +1,14 @@
+#pylsp
+pipx install 'python-lsp-server[all]' 
+
+# tsserver
+npm install -g typescript typescript-language-server
+
+#eslint_d
+npm install -g eslint_d
+
+#prettier
+npm install -g prettier prettier_d_slim
+
+# luaformatter
+luarocks install --server=https://luarocks.org/dev luaformatter 
