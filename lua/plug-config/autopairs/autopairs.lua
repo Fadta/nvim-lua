79118a1a48c017a9c34a17b5c84bfb0027ab7be5
Fadta <fvpan01@gmail.com>
2022-01-08 21:12:23 -0300

    removed deprecated setup call

--- lua/plug-config/autopairs/autopairs.lua
+++ lua/plug-config/autopairs/autopairs.lua
@@ -3,10 +3,6 @@ local npairs = require('nvim-autopairs')
 
 npairs.setup({
   disable_filetype = { "TelescopePrompt"},
-})
-
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp_autopairs.setup({
     map_cr = true, --  map <CR> on insert mode  
     map_complete = true, -- it will auto insert `(` after select function or method item
     auto_select = true -- automatically select the first item
