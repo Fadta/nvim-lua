db25ff8d6178a9a87bd696237a6d683dbe368299
Fadta <fvpan01@gmail.com>
2022-01-11 16:15:19 -0300

    Fixes #3

--- lua/lsp/settings.lua
+++ lua/lsp/settings.lua
@@ -52,11 +52,6 @@ local default_lsp_config = {on_attach = on_attach, capabilities}
 for serverName, config in pairs(servers) do
     local serverAvailable, server = lsp_installer_servers.get_server(serverName)
     if serverAvailable then
-        server:on_ready(function ()
-            -- unify tables, overwriting default_lsp_config with config if conflicts arise
-            local opts = vim.tbl_deep_extend('force', default_lsp_config, config)
-            server:setup(opts)
-        end)
         if not server:is_installed() then
             print('installing ' .. serverName)
             server:install()
@@ -67,6 +62,12 @@ for serverName, config in pairs(servers) do
     if nvim_lsp[serverName] == nil then
       dump(serverName)
     end
+
+    local opts = vim.tbl_deep_extend('force', default_lsp_config, config)
+
+    -- Temporal fix for LSP diagnostics working only on buffer open
+    -- look for differences at setup vs setup_lsp
+    server:setup_lsp(opts)
 
     -- Black magic ^-^
     vim.cmd [[ do User LspAttachBuffers ]]
